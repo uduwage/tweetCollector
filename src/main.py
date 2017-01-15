5b981f5f2e90151a4cfc898b2ea58b63e9909e7e
Anuradha Uduwage <uduw0001@umn.edu>
2017-01-15 13:27:41 -0500

    adding README

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 from tweetCollector import TweetCollector
-#https://github.com/suraj-deshmukh/get_tweets/blob/master/tweets-by-daterange.py
 def main():
     tc = TweetCollector()
     tc.harvestTweets()
